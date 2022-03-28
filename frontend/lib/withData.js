1ae66a68bb5687adce4c2a324ec487dadb2c5656
liviu-ene <remember5n@gmail.com>
2022-03-29 02:51:49 +0300

    Update withData.js

--- frontend/lib/withData.js
+++ frontend/lib/withData.js
@@ -29,7 +29,9 @@ function createClient({ headers, initialState }) {
           origin: 'https://clothify.vercel.app'
         },
         // pass the headers along from this request. This enables SSR with logged in state
-        headers,
+        headers : {
+          'Access-Control-Allow-Origin': 'https://clothify.vercel.app',
+        },
       }),
     ]),
     cache: new InMemoryCache({
