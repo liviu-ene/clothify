1d3d6a07f005d90e8c1da9640bfa72f1304d2b6e
liviu-ene <remember5n@gmail.com>
2022-03-28 16:47:46 +0300

    Update User.js
    test

--- frontend/components/User.js
+++ frontend/components/User.js
@@ -35,5 +35,37 @@ export const CURRENT_USER_QUERY = gql`
 
 export function useUser() {
   const { data } = useQuery(CURRENT_USER_QUERY);
-  return data?.authenticatedItem;
+  return {
+    "__typename": "User",
+    "id": "60d116b1c44a614e1c2a8f8d",
+    "email": "remember5n@gmail.com",
+    "name": "Ene Liviu",
+    "role": {
+        "__typename": "Role",
+        "canManageProducts": true,
+        "canManageCart": true,
+        "canManageOrders": true
+    },
+    "cart": [
+        {
+            "__typename": "CartItem",
+            "id": "6240b156b8ad9a566c0426f2",
+            "quantity": 1,
+            "product": {
+                "__typename": "Product",
+                "id": "6137f259f3be834c1831fab4",
+                "price": 53334,
+                "name": "Yeezy Boost 350",
+                "description": "signed by kanye himself",
+                "photo": {
+                    "__typename": "ProductImage",
+                    "image": {
+                        "__typename": "CloudinaryImage_File",
+                        "publicUrlTransformed": "https://res.cloudinary.com/dpol6zi0q/image/upload/v1631056473/sickfits/6137f258f3be834c1831fab2.webp"
+                    }
+                }
+            }
+        }
+    ]
+}
 }
